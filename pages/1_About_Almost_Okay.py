ce1d53fd13bcb201594e71baa6028dccb6c3f297
Penelope02 <131906780+Penelope02@users.noreply.github.com>
2025-10-23 15:30:43 +0200

    Update 1_About_Almost_Okay.py

---
+++ pages/1_About_Almost_Okay.py
@@ -0,0 +1,8 @@
+st.write(
+    """Finding therapy in the Netherlands can be confusing — prices are often unclear, and waiting times can be long.
+    
+Almost Okay was created to bring a little clarity (and calm) to the process.  
+By listing transparent information in one place, we hope to make mental health support feel more accessible and human.
+
+💛 We believe being almost okay is still a step towards being okay."""
+)
